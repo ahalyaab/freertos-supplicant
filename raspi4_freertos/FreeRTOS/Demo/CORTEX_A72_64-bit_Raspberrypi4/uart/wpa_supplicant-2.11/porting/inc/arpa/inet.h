eaac240f344ac81e9115f056dbfa1be3b76e8f28
ahalyaab <ahalya.bandaru@capgemini.com>
2025-11-07 15:49:19 +0530

    Incomplete struct reference in inet_ntoa from arpa/inet.h     - included netinet/in.h to define struct in_addr before use     - added minimal inet_addr, inet_ntoa, and inet_aton stub implementations

--- raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/arpa/inet.h
+++ raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/arpa/inet.h
@@ -1,15 +1,16 @@
-#ifndef _ARPA_INET_H_
-#define _ARPA_INET_H_
+#ifndef _FREERTOS_ARPA_INET_H_
+#define _FREERTOS_ARPA_INET_H_
 
+#include <stdint.h>
 #include <netinet/in.h>
 
 /*
- * Minimal stub for FreeRTOS build of wpa_supplicant.
- * These are placeholders for standard inet_xxx functions.
+ * Minimal stub for <arpa/inet.h>
  */
 
 static inline unsigned int inet_addr(const char *cp) { (void)cp; return 0; }
 static inline const char *inet_ntoa(struct in_addr in) { (void)in; return "0.0.0.0"; }
 static inline int inet_aton(const char *cp, struct in_addr *inp) { (void)cp; (void)inp; return 1; }
 
-#endif /* _ARPA_INET_H_ */
+#endif /* _FREERTOS_ARPA_INET_H_ */
+
