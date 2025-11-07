e2eb86e9a8105d876b54ce8be9fa105456365d7c
ahalyaab <ahalya.bandaru@capgemini.com>
2025-11-07 15:50:32 +0530

    Incomplete type 'struct in_addr' in inet_ntoa     - added definitions for struct in_addr and struct sockaddr_in     - added byte order helper functions (htons, ntohs, htonl, ntohl)     - defined INADDR_ANY, INADDR_BROADCAST, and AF_INET constants

--- raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/netinet/in.h
+++ raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/netinet/in.h
@@ -1,28 +1,47 @@
-#ifndef _NETINET_IN_H_
-#define _NETINET_IN_H_
+#ifndef _FREERTOS_NETINET_IN_H_
+#define _FREERTOS_NETINET_IN_H_
 
 /*
  * Minimal FreeRTOS stub for <netinet/in.h>
+ * Provides struct in_addr and sockaddr_in used by arpa/inet.h and other sources.
  */
 
-#include <sys/socket.h>
+#include <stdint.h>
 
-typedef unsigned short in_port_t;
-typedef unsigned int in_addr_t;
+/* basic types */
+typedef uint16_t in_port_t;
+typedef uint32_t in_addr_t;
 
-/* Byte order stubs */
-static inline unsigned short htons(unsigned short x) { return ((x << 8) | (x >> 8)); }
-static inline unsigned short ntohs(unsigned short x) { return htons(x); }
-static inline unsigned int htonl(unsigned int x) {
+/* IPv4 address structure */
+struct in_addr {
+    in_addr_t s_addr;
+};
+
+/* sockaddr_in for IPv4 sockets */
+struct sockaddr_in {
+    uint16_t sin_family;
+    uint16_t sin_port;
+    struct in_addr sin_addr;
+    unsigned char sin_zero[8];
+};
+
+/* Byte order helpers (simple stubs) */
+static inline uint16_t htons(uint16_t x) { return (uint16_t)((x << 8) | (x >> 8)); }
+static inline uint16_t ntohs(uint16_t x) { return htons(x); }
+static inline uint32_t htonl(uint32_t x) {
     return ((x << 24) | ((x << 8) & 0x00FF0000) | ((x >> 8) & 0x0000FF00) | (x >> 24));
 }
-static inline unsigned int ntohl(unsigned int x) { return htonl(x); }
+static inline uint32_t ntohl(uint32_t x) { return htonl(x); }
 
-/* Address constants */
+/* constants */
 #ifndef INADDR_ANY
-#define INADDR_ANY       ((in_addr_t)0x00000000)
+#define INADDR_ANY ((in_addr_t)0x00000000)
 #endif
-
 #define INADDR_BROADCAST ((in_addr_t)0xffffffff)
 
-#endif /* _NETINET_IN_H_ */
+#ifndef AF_INET
+#define AF_INET 2
+#endif
+
+#endif /* _FREERTOS_NETINET_IN_H_ */
+
