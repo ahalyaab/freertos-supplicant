86c4a0b9aa19a6ee9aa8013f451a7a801222bd3a
ahalyaab <ahalya.bandaru@capgemini.com>
2025-11-12 17:31:01 +0530

    fix(common): add BSD-style struct ip definition for DHCP compatibility

--- raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/netinet/ip.h
+++ raspi4_freertos/FreeRTOS/Demo/CORTEX_A72_64-bit_Raspberrypi4/uart/wpa_supplicant-2.11/porting/inc/netinet/ip.h
@@ -3,6 +3,24 @@
 
 #include <stdint.h>
 
+struct ip {
+    uint8_t  ip_hl:4;
+    uint8_t  ip_v:4;
+    uint8_t  ip_tos;
+    uint16_t ip_len;
+    uint16_t ip_id;
+    uint16_t ip_off;
+    uint8_t  ip_ttl;
+    uint8_t  ip_p;
+    uint16_t ip_sum;
+    uint32_t ip_src;
+    uint32_t ip_dst;
+};
+
+
+
+
+
 /* Minimal IPv4 header definition for FreeRTOS/lwIP build */
 struct iphdr {
     uint8_t  ihl:4;
